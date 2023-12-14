91000f42bd8919e221931890b37942d5b0412631
Jonrork5 <jonrork5@gmail.com>
2023-12-13 22:27:58 -0500

    I did it!

---
+++ SDknowledgecheck2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many records do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
+
+        var recordList = new List<Banana>(); 
+
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            var banana = new Banana();
+
+            Console.WriteLine("Enter the calories: ");
+            banana.Calories = int.Parse(Console.ReadLine());
+
+            recordList.Add(banana);
+        }
+
+        foreach (var record in recordList)
+        {
+            Console.WriteLine(record.ToString());
+        }
+    }
+}
+
+class Fruit
+{
+    public int Calories { get; set; }
+}
+
+class Banana : Fruit
+{
+    public override string ToString()
+    {
+        return $"Calories: {Calories}";
+    }
+}
